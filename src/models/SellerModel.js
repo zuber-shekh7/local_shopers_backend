8edf60e4774cc120e0d0aca30ab5da4637241aeb
Zuber Shekh <zuber.shekh7@gmail.com>
2022-01-02 20:28:53 +0530

    add seller model

---
+++ src/models/SellerModel.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const SellerSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    mobile: {
+      type: String,
+      trim: true,
+      unique: true,
+      sparse: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+SellerSchema.methods.authenticate = async function (password) {
+  return await bcrypt.compareSync(password, this.password);
+};
+
+SellerSchema.pre("save", async function (next) {
+  if (this.isModified) {
+    const hashPassword = await bcrypt.hashSync(this.password, 10);
+    this.password = hashPassword;
+  }
+  return next();
+});
+
+export default mongoose.model("Seller", SellerSchema);
