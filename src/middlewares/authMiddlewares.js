a2c55d69663302ecbb169f81a26d11ca0788c780
Zuber Shekh <zuber.shekh7@gmail.com>
2022-01-03 15:27:37 +0530

    add seller middleware

--- src/middlewares/authMiddlewares.js
+++ src/middlewares/authMiddlewares.js
@@ -19,4 +19,35 @@ const authenticate = (req, res, next) => {
   return next();
 };
 
-export { authenticate };
+const authenticateSeller = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res
+      .status(403)
+      .json({ message: "A Token is required for authentication" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.seller = decoded;
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  return next();
+};
+
+const allowSellerOnly = (req, res, next) => {
+  const seller = req.seller;
+
+  if (seller && seller.isSeller) {
+    return next();
+  }
+
+  return res.status(401).json({
+    message: "You are not authorised to access.",
+  });
+};
+
+export { authenticate, authenticateSeller, allowSellerOnly };
