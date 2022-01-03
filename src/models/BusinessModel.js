a541a99e17063fdf2ae71748a59d78775b2e0649
Zuber Shekh <zuber.shekh7@gmail.com>
2022-01-03 15:27:12 +0530

    add business model

---
+++ src/models/BusinessModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const BusinessSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Business", BusinessSchema);
