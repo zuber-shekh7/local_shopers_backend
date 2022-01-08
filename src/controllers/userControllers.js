9359cccdb1b316558f26cd586301b403817ba782
Zuber Shekh <zuber.shekh7@gmail.com>
2022-01-09 00:19:37 +0530

    add google based authentication

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -2,6 +2,7 @@ import User from "../models/UserModel.js";
 import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
 import { validationResult } from "express-validator";
+import { OAuth2Client } from "google-auth-library";
 
 const userLogin = asyncHandler(async (req, res) => {
   const errors = validationResult(req);
@@ -108,4 +109,71 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   return res.status(400).json({ message: "Invalid user id" });
 });
 
-export { userLogin, userSignup, getUserProfile, updateUserProfile };
+const userGoogleLogin = asyncHandler(async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400);
+    return res.json({ errors: errors.array() });
+  }
+
+  const token = req.body.token;
+  const client = new OAuth2Client(process.env.GOOGLE_OAUTH_CLIENT_ID);
+
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGLE_OAUTH_CLIENT_ID,
+  });
+
+  const {
+    email,
+    email_verified,
+    given_name: firstName,
+    family_name: lastName,
+  } = ticket.getPayload();
+
+  if (email_verified) {
+    const existuser = await User.findOne({ email: email }).select("-password");
+
+    if (existuser) {
+      const token = jwt.sign(
+        { id: existuser._id, isAdmin: existuser.isAdmin },
+        process.env.SECRET,
+        { expiresIn: "30d" }
+      );
+
+      return res.json({ token, user: existuser });
+    }
+
+    const password = email + process.env.SECRET;
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET,
+      { expiresIn: "30d" }
+    );
+
+    return res.json({
+      token,
+      user: await User.findById(user._id).select("-password"),
+    });
+  }
+
+  return res.status(400).json({
+    message: "Your email address in not verified, please verify you email.",
+  });
+});
+
+export {
+  userLogin,
+  userSignup,
+  getUserProfile,
+  updateUserProfile,
+  userGoogleLogin,
+};
