701cf5a6da866ccc2cc34bf7ebcb2e79819b451a
Zuber Shekh <zuber.shekh7@gmail.com>
2022-01-10 19:07:30 +0530

    update product model

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -13,7 +13,7 @@ const ProductSchema = mongoose.Schema(
       required: true,
     },
     price: {
-      type: mongoose.Schema.Types.Decimal128,
+      type: Number,
       required: true,
     },
     quantity: {
