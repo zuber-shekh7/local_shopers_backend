722b08a0d98c4c414fe8780e3cc181d2dc26fa8b
Zuber Shekh <zuber.shekh7@gmail.com>
2022-01-24 14:04:59 +0530

    add remove from wish list api

--- src/controllers/wishListControllers.js
+++ src/controllers/wishListControllers.js
@@ -90,4 +90,48 @@ const addToWishList = asyncHandler(async (req, res) => {
   });
 });
 
-export { getWishList, addToWishList };
+const removeFromWishList = asyncHandler(async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400);
+    return res.json({ errors: errors.array() });
+  }
+
+  const wish_list_id = req.body.wish_list_id;
+  const product_id = req.body.product_id;
+
+  if (!mongoose.Types.ObjectId.isValid(wish_list_id)) {
+    return res.status(400).json({
+      message: "Invalid wish list id",
+    });
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(product_id)) {
+    return res.status(400).json({
+      message: "Invalid product id",
+    });
+  }
+
+  const wishList = await WishList.findById(wish_list_id).populate("products");
+
+  if (wishList) {
+    const product = await Product.findById(product_id);
+
+    if (product) {
+      wishList.products.pull(product);
+      wishList.save();
+      return res.json({ wishList });
+    }
+
+    return res.status(400).json({
+      message: "Invalid wish list id",
+    });
+  }
+
+  return res.status(400).json({
+    message: "Invalid wish list id",
+  });
+});
+
+export { getWishList, addToWishList, removeFromWishList };
--- src/routes/wishListRoutes.js
+++ src/routes/wishListRoutes.js
@@ -3,6 +3,7 @@ import { body, query } from "express-validator";
 import {
   addToWishList,
   getWishList,
+  removeFromWishList,
 } from "../controllers/wishListControllers.js";
 import { authenticate } from "../middlewares/authMiddlewares.js";
 
@@ -22,6 +23,16 @@ router.post(
     body("product_id").exists().notEmpty().isString(),
   ],
   addToWishList
+);
+
+router.post(
+  "/remove",
+  [
+    authenticate,
+    body("wish_list_id").exists().notEmpty().isString(),
+    body("product_id").exists().notEmpty().isString(),
+  ],
+  removeFromWishList
 );
 
 export default router;
