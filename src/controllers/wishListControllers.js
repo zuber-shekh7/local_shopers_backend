66a9a323908399b7cbf80b40070423ffa63221c6
Zuber Shekh <zuber.shekh7@gmail.com>
2022-01-24 13:59:24 +0530

    add add to wishlist api

--- src/controllers/wishListControllers.js
+++ src/controllers/wishListControllers.js
@@ -3,6 +3,7 @@ import WishList from "../models/WishListModel.js";
 import asyncHandler from "express-async-handler";
 import { validationResult } from "express-validator";
 import mongoose from "mongoose";
+import Product from "../models/ProductModel.js";
 
 const getWishList = asyncHandler(async (req, res) => {
   const errors = validationResult(req);
@@ -24,14 +25,20 @@ const getWishList = asyncHandler(async (req, res) => {
 
   if (user) {
     if (user.wishList) {
-      return res.json({ wishList: user.wishList });
+      return res.json({
+        wishList: await WishList.findById(user.wishList._id).populate(
+          "products"
+        ),
+      });
     }
 
     const wishList = await WishList.create({});
     user.wishList = wishList;
     user.save();
 
-    return res.json({ wishList });
+    return res.json({
+      wishList: await WishList.findById(wishList._id).populate("products"),
+    });
   }
 
   return res.status(400).json({
@@ -39,4 +46,48 @@ const getWishList = asyncHandler(async (req, res) => {
   });
 });
 
-export { getWishList };
+const addToWishList = asyncHandler(async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400);
+    return res.json({ errors: errors.array() });
+  }
+
+  const wish_list_id = req.body.wish_list_id;
+  const product_id = req.body.product_id;
+
+  if (!mongoose.Types.ObjectId.isValid(wish_list_id)) {
+    return res.status(400).json({
+      message: "Invalid wish list id",
+    });
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(product_id)) {
+    return res.status(400).json({
+      message: "Invalid product id",
+    });
+  }
+
+  const wishList = await WishList.findById(wish_list_id).populate("products");
+
+  if (wishList) {
+    const product = await Product.findById(product_id);
+
+    if (product) {
+      wishList.products.push(product);
+      wishList.save();
+      return res.json({ wishList });
+    }
+
+    return res.status(400).json({
+      message: "Invalid wish list id",
+    });
+  }
+
+  return res.status(400).json({
+    message: "Invalid wish list id",
+  });
+});
+
+export { getWishList, addToWishList };
--- src/routes/wishListRoutes.js
+++ src/routes/wishListRoutes.js
@@ -1,6 +1,9 @@
 import express from "express";
-import { query } from "express-validator";
-import { getWishList } from "../controllers/wishListControllers.js";
+import { body, query } from "express-validator";
+import {
+  addToWishList,
+  getWishList,
+} from "../controllers/wishListControllers.js";
 import { authenticate } from "../middlewares/authMiddlewares.js";
 
 const router = express.Router();
@@ -9,6 +12,16 @@ router.get(
   "/",
   [authenticate, query("user_id").exists().notEmpty().isString()],
   getWishList
+);
+
+router.post(
+  "/add",
+  [
+    authenticate,
+    body("wish_list_id").exists().notEmpty().isString(),
+    body("product_id").exists().notEmpty().isString(),
+  ],
+  addToWishList
 );
 
 export default router;
