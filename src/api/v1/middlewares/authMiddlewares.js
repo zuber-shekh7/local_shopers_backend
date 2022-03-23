670fd7e7bb0ab67de2850c60c2e47b98c79202d7
Zuber Shekh <zuber.shekh7@gmail.com>
2022-03-23 21:06:45 +0530

    add admin authenticate middleware

--- src/api/v1/middlewares/authMiddlewares.js
+++ src/api/v1/middlewares/authMiddlewares.js
@@ -40,6 +40,26 @@ const authenticateSeller = (req, res, next) => {
   }
 };
 
+const authenticateAdmin = (req, res, next) => {
+  let token = req.cookies.token;
+
+  // validating bearer token
+  if (!token) {
+    return res
+      .status(403)
+      .json({ message: "A Token is required for authentication" });
+  }
+
+  try {
+    // decoding jwt token
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.admin = decoded;
+    return next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
 const allowAdminOnly = (req, res, next) => {
   const user = req.user;
 
@@ -64,4 +84,10 @@ const allowSellerOnly = (req, res, next) => {
   });
 };
 
-export { authenticate, authenticateSeller, allowSellerOnly, allowAdminOnly };
+export {
+  authenticate,
+  authenticateSeller,
+  authenticateAdmin,
+  allowSellerOnly,
+  allowAdminOnly,
+};
